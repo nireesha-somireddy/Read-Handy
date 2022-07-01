f7096fa089b0cd4d82d0e0d880ed22dc6d28326e
Nireesha <somireddynireesha2247@gmail.com>
2022-06-30 20:05:23 -1200

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
+    
         </IonHeader>
         <ExploreContainer />
+       <h1 id='txt'><b> Welcome to Readers Icon</b></h1>
+       <h3 id='txt1'><b>Knowledge is power</b></h3>
+
+       <IonButton expand="full" shape="round" id="btn" color='biscuit'>Sign Up</IonButton>
+       <IonButton expand="full" href='/login' shape="round" id="btn1" color="biscuit">Sign In</IonButton>
       </IonContent>
     </IonPage>
   );
---
+++ src/pages/login.tsx
@@ -0,0 +1,31 @@
+import { IonContent, IonHeader, IonPage, IonInput, IonButton, IonCard} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './login.css';
+
+
+const Login: React.FC = () => {
+  return (
+    <IonPage >
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+        </IonHeader>
+        <ExploreContainer />
+       <h1 id='txt'><b> Welcome back</b></h1>
+       <IonCard>
+        <IonInput class="input" placeholder="Enter your Email"/> 
+        <IonInput class="input1" placeholder="password"/>
+      
+        <p>Forgot Password?</p>
+       <IonButton expand="full" shape="round" color='biscuit' id='signin'>sign In</IonButton>
+        <p>Don't have an account?Sign Up</p>
+
+        <p>----Or sign in with----</p>
+       <IonButton  shape="round" color='biscuit' id='btnf'>Facebook</IonButton>&nbsp; &nbsp;
+       <IonButton  shape="round"color="biscuit" id='btng'>Google</IonButton>
+       </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
