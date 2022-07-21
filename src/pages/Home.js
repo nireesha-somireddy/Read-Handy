8c22c8650938e5b29bbbeda702fa262b566bcbcb
Nireesha <somireddynireesha2247@gmail.com>
2022-07-21 15:38:48 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonButton, IonGrid,IonImg,useIonLoading, IonCard, IonRow, IonCol} from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonButton, IonGrid, IonImg, IonRow } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-
 import './Home.css';
-
 const Home = () => {
-
   return (
     <IonPage>
       <IonContent color='dark' className='ion-content'>
@@ -12,17 +9,17 @@ const Home = () => {
         </IonHeader>
         <ExploreContainer />
         <IonGrid>
-        <IonRow>
-        <IonImg src="../assets/suplogo.jpg" className="homeimg">{" "}</IonImg>
-        </IonRow>
-        <IonRow className='title'> Read Handy </IonRow>
-        <IonRow>
-             <h1 className='wel'>Welcome </h1>
-       </IonRow>
-      <IonRow>
-       <IonButton  routerLink='/login'  id="btn1" color="danger" align='center'>Get Started</IonButton>
-       </IonRow>
-       </IonGrid>
+          <IonRow>
+            <IonImg src="../assets/suplogo.jpg" className="homeimg">{" "}</IonImg>
+          </IonRow>
+          <IonRow className='title'> Read Handy </IonRow>
+          <IonRow>
+            <h1 className='wel'>Welcome </h1>
+          </IonRow>
+          <IonRow>
+            <IonButton routerLink='/login' id="btn1" color="danger" align='center'>Get Started</IonButton>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
